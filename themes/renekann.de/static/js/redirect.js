07d7a11716dd39e72f6fba93e6f138e63504ef0a
René Kann <mail@renekann.de>
2019-07-25 14:02:47 +0200

    Language redirect

---
+++ themes/renekann.de/static/js/redirect.js
@@ -0,0 +1,34 @@
+if (localStorage.getItem('language') == null) {
+  var userLang = navigator.language || navigator.userLanguage;
+  var languageCode = userLang.split("-")[0].toLowerCase();
+
+  localStorage.setItem('language', languageCode);
+  redirect(languageCode);
+}
+
+function redirect(actuallang) {
+
+  var path = window.location.pathname
+  var protocol = window.location.protocol
+  var baseUrl = window.location.hostname
+  var newPath = "/en"
+
+  if (path.includes(actuallang)) {
+    return
+  }
+
+  if (baseUrl == "localhost") {
+    baseUrl = baseUrl + ":1313"
+  }
+
+  switch (actuallang) {
+    case 'de':
+      newPath = '/de';
+      break;
+    default:
+      newPath = '/en';
+      break;
+  }
+
+  window.location.replace(protocol + "//" + baseUrl + newPath);
+}
